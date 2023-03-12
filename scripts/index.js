67a83d70f8712a91df42f8ff8bc3138888498b7a
Alena Nikolaevna <magnittyt@mail.ru>
2023-03-13 04:03:38 +0700

    Проектная работа 6

--- scripts/index.js
+++ scripts/index.js
@@ -190,7 +190,18 @@ function handleFormSubmitAdd(evt) {
 
 formElementAdd.addEventListener('submit', handleFormSubmitAdd); // обработчик кнопки "создать"
 
+/*********************** */
+const validationContainer = {
+  formSelector: '.popup__form-edit-container',
+  inputSelector: '.popup__form-item',
+  submitButtonSelector: '.popup__form-button-submit',
+  inactiveButtonClass: 'popup__form-button-submit_disabled',
+  inputErrorClass: 'popup__form-item_type_error',
+  errorClass: 'popup__form-item-error_visible'
+}
+
 enableValidation(validationContainer);
+
 
 
 
