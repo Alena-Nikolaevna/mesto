fb4a640152995c5ea35c2def5e47e6ef0d95bd44
Alena Nikolaevna <magnittyt@mail.ru>
2023-04-05 14:56:49 +0700

    Проектная работа - 7

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -3,6 +3,11 @@ export default class FormValidator {
     constructor(options, formElement) {
       this._options = options;
       this._formElement = formElement;
+
+      this._inputList = Array.from(this._formElement.querySelectorAll(this._options.inputSelector));
+
+      // Найдём в текущей форме кнопку отправки
+      this._buttonElement = this._formElement.querySelector(this._options.submitButtonSelector);
     }
   
     enableValidation() {
@@ -46,47 +51,42 @@ export default class FormValidator {
       }
     }
   
-    _hasInvalidInput(inputList) {
-      return inputList.some((inputElement) => {
+    _hasInvalidInput() {
+      return this._inputList.some((inputElement) => {
       return !inputElement.validity.valid;
       });
     }
 
-    _toggleButtonState(inputList, buttonElement) {
+    _toggleButtonState() {
       // Если есть хотя бы один невалидный инпут
-      if(this._hasInvalidInput(inputList)) {
+      if(this._hasInvalidInput()) {
         // сделай кнопку неактивной
-        buttonElement.classList.add(this._options.inactiveButtonClass);
-        buttonElement.setAttribute('disabled', true);
+        this._buttonElement.classList.add(this._options.inactiveButtonClass);
+        this._buttonElement.setAttribute('disabled', true);
 
       } else {
         // иначе сделай кнопку активной
-        buttonElement.classList.remove(this._options.inactiveButtonClass);
-        buttonElement.removeAttribute('disabled');
+        this._buttonElement.classList.remove(this._options.inactiveButtonClass);
+        this._buttonElement.removeAttribute('disabled');
       }
     }
   
     _setEventListeners() {
-      const inputList = Array.from(this._formElement.querySelectorAll(this._options.inputSelector));
-      
-      // Найдём в текущей форме кнопку отправки
-      const buttonElement = this._formElement.querySelector(this._options.submitButtonSelector);
-        
+
       this._formElement.addEventListener('reset', () => { // собыите `reset` происходит когда вызывается `reset` у формы
         setTimeout(() => {  // добавим таймаут, чтобы `toggleButtonState` вызвался уже после сохранения формы
-          this._toggleButtonState(inputList, buttonElement), 0 })
+          this._toggleButtonState(), 0 })
       })
     
-    
-      inputList.forEach((inputElement) => {
+      this._inputList.forEach((inputElement) => {
           inputElement.addEventListener('input', () => {
                 this._isValid(inputElement);
                 // Вызовем toggleButtonState и передадим ей массив полей и кнопку
-                this._toggleButtonState(inputList, buttonElement);
+                this._toggleButtonState();
                });    
       });
     
-          this._toggleButtonState(inputList, buttonElement);
+          this._toggleButtonState();
       }
     }
 
@@ -113,3 +113,7 @@ export default class FormValidator {
        this._disabledRemoveButtonElement(buttonElement);
      }
     }*/
+
+    /****************************************************** */
+     /*а для того ,чтобы проверить, потерялся ли контекс его всегда можно выводить в консоль
+       (например перед вызовом this._isValid() написать лог this)*/
